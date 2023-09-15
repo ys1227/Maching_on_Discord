fd2d38523143ef0c38dcdf21ade87998846e52f4
ys1227 <y.saki1227@outlook.jp>
2023-09-16 01:37:54 +0900

    fix render_erros_of_docker

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,4 @@ bundle install
 yarn install
 yarn build:css # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
-bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
+bundle bundle exec rails db:migrat # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います
