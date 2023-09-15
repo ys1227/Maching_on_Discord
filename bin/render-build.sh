7dfa395ddc219b1158c1f082e5b2b9dc76a36bd1
ys1227 <y.saki1227@outlook.jp>
2023-09-16 02:06:13 +0900

    fix render-build.sh_migrate_command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ bundle install
 yarn install
 yarn build:css # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
-bin/rails db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います
+bin/rails db:migrate RAILS_ENV=development
+ # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います
