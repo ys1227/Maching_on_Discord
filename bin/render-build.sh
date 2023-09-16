ec2097e5a464be4479bb968764280839c12938f7
ys1227 <y.saki1227@outlook.jp>
2023-09-16 11:49:19 +0900

    fix db_errors_of_development

---
+++ bin/render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bin/rails db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
